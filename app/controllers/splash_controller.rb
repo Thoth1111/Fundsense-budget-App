c00cb8633ab63ba4dc8a361ee8d2546d6fc90db9
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-29 11:03:26 +0300

    Add splash screen controller and set as app root

---
+++ app/controllers/splash_controller.rb
@@ -0,0 +1,10 @@
+class SplashController < ApplicationController
+
+    def index
+        if user_signed_in?
+            redirect_to groups_path
+        else
+            render :index
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
+  root "splash#index"
+
   resources :entries
   resources :groups
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+ 
 end
