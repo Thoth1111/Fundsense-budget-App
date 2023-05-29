1599bb4818376d933657c10b80ee34a917e138a6
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-29 15:37:56 +0300

    Enable user authentication for app access

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :trackable, :registerable, :timeoutable, :lockable, :confirmable,
          :recoverable, :rememberable, :validatable
+
+  has_many :groups, dependent: :destroy
+  has_many :entries, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 50 }
+  
 end
