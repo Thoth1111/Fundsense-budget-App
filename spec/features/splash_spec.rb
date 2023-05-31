e2f6eb7bf60ddc747980cde30826e0e9ec9cd62b
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-31 20:35:22 +0300

    Add integration tests for splash screen

---
+++ spec/features/splash_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Splash', type: :feature do
+    describe 'root' do
+        before(:all) do
+            Capybara.reset_sessions!
+        end
+
+        before(:each) do
+            visit root_path
+        end
+
+        it 'should display the page name' do
+            expect(page).to have_content('Fundsense')
+        end
+
+        it 'should display the page slogan' do
+            expect(page).to have_content('personal finance management in your hands')
+        end
+
+        it 'should have a link to log in' do
+            expect(page).to have_link('Log In')
+        end
+
+        it 'should have a link to sign up' do
+            expect(page).to have_link('Sign Up')
+        end
+    end
+end
