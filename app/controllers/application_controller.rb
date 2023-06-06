be0dd959caace58550e0a73a6826157e317545ce
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-06 13:15:01 +0300

    Modify icon handling on group creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # It provides common functionality and serves as a parent class for other controllers.
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 # Group model.
 class Group < ApplicationRecord
+  after_commit :update_icon, on: :create
+
   belongs_to :user
   has_one_attached :icon
   has_many :entries_groups, dependent: :destroy
@@ -9,4 +11,15 @@ class Group < ApplicationRecord
 
   validates :name, presence: true
   validates :icon, presence: true
+
+  private
+
+  def update_icon
+    if icon.attached?
+      update_column(:icon, icon.url)
+    else
+      self.icon = icon
+      save
+    end
+  end
 end
