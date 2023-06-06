2322710c1612c6b27faf7ac293888db4e893d9db
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-06 21:20:57 +0300

    Add cloudinary cloud name to application.rb

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'cloudinary'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
